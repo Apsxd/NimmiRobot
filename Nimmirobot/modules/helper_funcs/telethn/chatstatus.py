7ad363be484a4798e50de899f4b2f9e3cf0a7015
Basimon <74089102+basimonpp@users.noreply.github.com>
2021-03-18 20:35:44 +0530

    Update chatstatus.py

--- Nimmirobot/modules/helper_funcs/telethn/chatstatus.py
+++ Nimmirobot/modules/helper_funcs/telethn/chatstatus.py
@@ -44,11 +44,11 @@ async def is_user_admin(user_id: int, chat_id):
 
 async def saitama_is_admin(chat_id: int):
     status = False
-    AstrakoBot = await telethn.get_me()
+    Nimmirobot = await telethn.get_me()
     async for user in telethn.iter_participants(
         chat_id, filter=ChannelParticipantsAdmins
     ):
-        if AstrakoBot.id == user.id:
+        if Nimmirobot.id == user.id:
             status = True
             break
     return status
