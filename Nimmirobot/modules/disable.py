b96c42a35b44ceb0de1c926f8a1042a04ba119e1
Lucido <74089102+Basi-mon@users.noreply.github.com>
2021-05-22 09:43:59 +0530

    Update disable.py

--- Nimmirobot/modules/disable.py
+++ Nimmirobot/modules/disable.py
@@ -348,7 +348,7 @@ if is_module_loaded(FILENAME):
     • `/listcmds`*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "COMMAND"
+    __mod_name__ = "COMMANDS"
 
 else:
     DisableAbleCommandHandler = CommandHandler
