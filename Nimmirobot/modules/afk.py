bae66a694fda62b4db1a22f8d091dcecbacaa384
Lucido <74089102+Basi-mon@users.noreply.github.com>
2021-05-22 09:30:25 +0530

    Update afk.py

--- Nimmirobot/modules/afk.py
+++ Nimmirobot/modules/afk.py
@@ -38,7 +38,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} the devil had gone 😈👿{}".format(fname, notice))
+        update.effective_message.reply_text("{} is afk now {}".format(fname, notice))
     except BadRequest:
         pass
 
